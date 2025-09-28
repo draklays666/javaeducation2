d44e16a2a9f1364c8ffe66ccf586745eb438ae62
draklays666 <sasaykonstantinov5@gmail.com>
2025-09-28 13:51:32 +0300

    project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int r = 1;
+        String person = "\uD83E\uDDD9\u200D";
+        int personLive = 3;
+        /* здесь необходимо рассказать про переполнение и про другие типы данных
+            int personLive = 2147483649; // мало ли кто-то захочет сделать ооочень много жизней
+         */
+
+        String monster = "\uD83E\uDDDF\u200D";
+        int sizeBoard = 3;
+        int personX = 1;
+        int personY = 3;
+
+        int step = 0;
+        // \n, \t - спец символ
+        String gamingField = "+ —— + —— + —— +\n"
+                + "|    |    | \uD83C\uDFE0 |\n"
+                + "+ —— + —— + —— +\n"
+                + "|    | " + monster + " |    |\n"
+                + "+ —— + —— + —— +\n"
+                + "| " + person + " |    |    |\n"
+                + "+ —— + —— + —— +";
+
+        System.out.println("Привет! Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
+
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.nextLine();
+        System.out.println("Ваш ответ:\t" + answer);
+
+        System.out.println("Выбери сложность игры(от 1 до 5):");
+        int difficultGame = sc.nextInt();
+        System.out.println("Выбранная сложность:\t" + difficultGame);
 
     }
 }
